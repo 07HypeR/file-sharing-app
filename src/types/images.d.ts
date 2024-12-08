42c500d104762a98dda7bbd30b3976d0dcbf3788
Abhik Baidya <abhikbaidya7@gmail.com>
2024-12-08 16:05:38 +0530

    Project set-up

---
+++ src/types/images.d.ts
@@ -0,0 +1,17 @@
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.ttf';
+
+declare module 'net' {
+  import TcpSockets from 'react-native-tcp-socket';
+  export = TcpSockets;
+}
+
+declare module 'tls' {
+  import TcpSockets from 'react-native-tcp-socket';
+  export const Server = TcpSockets.TLSServer;
+  export const TLSSocket = TcpSockets.TLSSocket;
+  export const connect = TcpSockets.connectTLS;
+  export const createServer = TcpSockets.createTLSServer;
+}
