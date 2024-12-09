5c1e339ba895b3cd94e1339306d47f8a00506f18
Abhik Baidya <abhikbaidya7@gmail.com>
2024-12-09 10:55:18 +0530

    Media permission added

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {Platform} from 'react-native';
+import React, {useEffect} from 'react';
 import Navigation from './src/navigation/Navigation';
+import {requestPhotoPermission} from './src/utils/Constants';
+import {checkFilePermissions} from './src/utils/libraryHelpers';
 
 const App = () => {
+  useEffect(() => {
+    requestPhotoPermission();
+    checkFilePermissions(Platform.OS);
+  }, []);
+
   return <Navigation />;
 };
 
