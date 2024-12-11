a7fbd286da0d5679cb909198f094dc024c02d18c
Abhik Baidya <abhikbaidya7@gmail.com>
2024-12-11 11:27:58 +0530

    CustomText added

--- src/components/home/Options.tsx
+++ src/components/home/Options.tsx
@@ -3,6 +3,7 @@ import React, {FC} from 'react';
 import {optionStyles} from '../../styles/optionsStyles';
 import Icon from '../global/Icon';
 import {Colors} from '../../utils/Constants';
+import CustomText from '../global/CustomText';
 
 const Options: FC<{
   isHome?: boolean;
@@ -22,6 +23,59 @@ const Options: FC<{
           color={Colors.primary}
           size={20}
         />
+        <CustomText
+          fontFamily="Okra-Medium"
+          style={{marginTop: 4, textAlign: 'center'}}>
+          Photo
+        </CustomText>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={optionStyles.subContainer}
+        onPress={() => handleUniversalPicker('file')}>
+        <Icon
+          name="musical-notes-sharp"
+          iconFamily="Ionicons"
+          color={Colors.primary}
+          size={20}
+        />
+        <CustomText
+          fontFamily="Okra-Medium"
+          style={{marginTop: 4, textAlign: 'center'}}>
+          Audio
+        </CustomText>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={optionStyles.subContainer}
+        onPress={() => handleUniversalPicker('file')}>
+        <Icon
+          name="folder-open"
+          iconFamily="Ionicons"
+          color={Colors.primary}
+          size={20}
+        />
+        <CustomText
+          fontFamily="Okra-Medium"
+          style={{marginTop: 4, textAlign: 'center'}}>
+          Files
+        </CustomText>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={optionStyles.subContainer}
+        onPress={() => handleUniversalPicker('file')}>
+        <Icon
+          name="contacts"
+          iconFamily="MaterialCommunityIcons"
+          color={Colors.primary}
+          size={20}
+        />
+        <CustomText
+          fontFamily="Okra-Medium"
+          style={{marginTop: 4, textAlign: 'center'}}>
+          Contacts
+        </CustomText>
       </TouchableOpacity>
     </View>
   );
